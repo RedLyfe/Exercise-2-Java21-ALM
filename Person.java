6ab682a511d68831f67cfb51306efe1013a24248
Panos <ioannidis_panos@live.se>
2022-08-26 17:27:36 +0200

    printed out object panos

--- Person.java
+++ Person.java
@@ -2,7 +2,7 @@
 
 public class Person {
     public static void main (String args[] ){
-        Panos panos = new Panos("panos", 22);
-        System.out.println();
+        Panos panos = new Panos("ngasdasdf", 22);
+        System.out.println(panos.getName());
     }
 }
